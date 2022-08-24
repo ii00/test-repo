3fbe180f539925e1389c074652e5505cb4c70211
btmn <ivayloivanov00@gmail.com>
2022-08-24 08:34:49 +0300

    Frontend (WP: Heroku setup), Backend (WP: Heroku setup)

--- api/config/development.py
+++ api/config/development.py
@@ -17,23 +17,18 @@ class Development(Common):
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     
     # Postgres  DB Settings
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql',
-    #         'NAME': os.getenv('DATABASE_NAME'),
-    #         'USER': os.getenv('DATABASE_USER'),
-    #         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-    #         'HOST': os.getenv('DATABASE_HOST'),
-    #         'PORT': os.getenv('DATABASE_PORT'),
-    #     }
-    # }
-    
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR + '/' + 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DATABASE_NAME'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('DATABASE_HOST'),
+            'PORT': os.getenv('DATABASE_PORT'),
         }
     }
+    
+
     
     DEBUG = True
     LOGGING = {
